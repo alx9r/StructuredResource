f6d14e3a5deb45787027c3ad10d08cf8f24c8250
alx9r <eggy.island9@gmail.com>
2017-09-09 08:50:46 -0700

    test for existence of help entries

--- publicApi.Tests.ps1
+++ publicApi.Tests.ps1
@@ -1,11 +1,32 @@
 Import-Module StructuredDscResourceCheck -Force
 
 Describe 'Public API' {
-    $r = Get-Command -Module StructuredDscResourceCheck
+    $commands = Get-Command -Module StructuredDscResourceCheck
     It 'exports some functions...' {
-        $r | measure | % Count | Should beGreaterThan 1
+        $commands | measure | % Count | Should beGreaterThan 1
     }
     It '...but not too many' {
-        $r | measure | % Count | Should beLessThan 10
+        $commands | measure | % Count | Should beLessThan 10
+    }
+    Context 'help' {
+        foreach ( $command in $commands )
+        {
+            Context $command.Name {
+                $help = $command | Get-Help
+                It 'has a synopsis' {
+                    $help.Synopsis | Should not match $command.Name
+                }
+                It 'has a description' {
+                    $help.Description | Should not beNullOrEmpty
+                }
+                It 'parameter <n> has a description' -TestCases @(
+                    $help.parameters.parameter | 
+                        % { @{ n=$_.Name; p=$_ } }
+                ) {
+                    param($n,$p)
+                    $p.Description | Should not beNullOrEmpty
+                }
+            }
+        }
     }
 }
