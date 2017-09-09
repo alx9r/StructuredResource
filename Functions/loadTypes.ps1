27ef3d35c8906bb73b55bb3c0f796cb83359a3fd
alx9r <eggy.island9@gmail.com>
2017-09-09 11:29:02 -0700

    rename files to structuredResourceTestCollection

--- Functions/loadTypes.ps1
+++ Functions/loadTypes.ps1
@@ -1,6 +1,6 @@
 @(
     'testParamsType.ps1'
     'structuredResourceTestType.ps1'
-    'testInstructionsType.ps1'
+    'structuredResourceTestCollectionType.ps1'
 ) |
 % { . "$($PSCommandPath | Split-Path -Parent)\$_" }
